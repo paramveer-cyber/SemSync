0e2f453af179652143b3e2f51851172dd4225fd0
Paramveer <68002445+paramveer-cyber@users.noreply.github.com>
2026-04-08 15:16:33 +0530

    Deployed url added to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://sem-sync-pearl.vercel.app"],
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
