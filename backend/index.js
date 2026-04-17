4dbade938768356a56bd9f10bb0c13d614e9d227
Paramveer <68002445+paramveer-cyber@users.noreply.github.com>
2026-04-17 12:45:24 +0530

    minor change in backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import app from "./src/index.js";
 const PORT = process.env.PORT ?? 3000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀  Server running at http://localhost:${server.address().port}`);
 });
 
